9af1d7a2facc207cb879ffbc69b762d6cf99dbdd
davidGBbr <davidgomesbrigido@gmail.com>
2023-07-25 11:10:30 -0300

    showing items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+import { TodoItem } from "@/types/TodoItem";
+import { useState } from "react";
+
 const Page = () => {
-  return <div></div>;
+  const [list, setList] = useState<TodoItem[]>([
+    { label: "Fazer dever de casa", checked: false },
+    { label: "Comprar o bolo", checked: false },
+  ]);
+  return (
+    <div className="w-screen h-screen flex flex-col items-center text-2xl">
+      <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
+      <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-500">
+        <input
+          type="text"
+          placeholder="O que deseja fazer?"
+          className="flex-1 border border-black p-3 text-2xl text-black rounded-md mr-3"
+        />
+        <button>Adicionar</button>
+      </div>
+
+      <ul className="w-full max-w-lg list-disc pl-5">
+        {list.map((item) => (
+          <li>
+            {item.label} -
+            <button className="hover:underline">[ deletar ]</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Page;
